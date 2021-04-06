640e27037933bd4df31892b82a051e81cac7bb7e
CGalko <cash.galko12@ncf.edu>
2021-04-05 21:58:52 -0400

    got the display function to work in js. Added more elements to the html to use the .show/hide function of jQuery. Need to modify css to add additional functionality & style.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,9 @@ var formSubmitHandler = function (event) {
 };
 
 var getWeather = function (city) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=56c8a3ef9b920710fdace6246f366751';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=56c8a3ef9b920710fdace6246f366751';
 
-    fetch(apiUrl)
+ fetch(apiUrl)
   .then(function (response) {
     return response.json();
 
@@ -35,40 +35,15 @@ var getWeather = function (city) {
   });
 }
 
-var displayWeather = function () {
-    var weatherContainer = document.getElementById('#weather-container');
-
-    for (var i = 0; i < data.length; i++) {
-        var cityNameHeader = document.createElement('h2');
-        var temper = document.createElement('p');
-        var wind = document.createElement('p');
-        var humidity = document.createElement('p');
-        var uvIndex = document.createElement('p');
-        cityNameHeader.textContent = data[i].name;
-        temper.textContent = data[i].main.temp;
-        wind.textContent = data[i].wind.speed;
-        humidity.textContent = data[i].main.humidity;
-        
-        weatherContainer.append(cityNameHeader);
-        weatherContainer.append(temper);
-        weatherContainer.append(wind);
-        weatherContainer.append(humidity);
-      }
-}
-  
-//     fetch(apiUrl)
-//       .then(function (response) {
-//         if (response.ok) {
-//           response.json()
-//           console.log(apiUrl);
-//         } else {
-//           alert('Error: ' + response.statusText);
-//         }
-//       })  
+var displayWeather = function (data, city) {
     
-//       .catch(function (error) {
-//         alert('Unable to connect to GitHub');
-//       });
-//   };
+    $('#divWeather').show();
+    
+    $('#hCity').text(data.name);    
+    $('#temp-display').text(data.main.temp);
+    $('#wind-display').text(data.wind.speed);
+    $('#humidity-display').text(data.wind.speed);
+    // $('#uvIndex-display').text(data.wind.speed);
+}
 
   userFormEl.addEventListener('submit', formSubmitHandler)
